d5eafbc2d19769cbff81eed40165bb2e74441f69
Raphael <40037381+rk-exxec@users.noreply.github.com>
2023-03-08 13:57:33 +0100

    Update for new inkscape command line params

--- convert_pdf_to_emf.bat
+++ convert_pdf_to_emf.bat
@@ -4,6 +4,9 @@ set targetdir=%1
 
 for /R %targetdir% %%i in (*.pdf) do (
 	echo Converting %%~ni.pdf to %%~ni.emf ...
-	inkscape --without-gui --file="%%i" --export-emf="%%~pi/%%~ni.emf"
+REM pre 0.92 inkscape cl options 
+REM	inkscape --without-gui --file="%%i" --export-emf="%%~pi/%%~ni.emf"
+REM current inkscape options
+	inkscape "%%i" --export-filename="%%~pi/%%~ni.emf"
 	echo Done
 )
